d4a0f54ace2e707e4019c4144edcee377c6e92fc
sayaoyuri <yuriantunes0000@gmail.com>
2023-07-25 22:07:31 -0300

    Feat(customers): implementing post /customers method

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -18,8 +18,28 @@ export const listCustomerById = async (req, res) => {
     
     if(customer.rowCount === 0) return res.sendStatus(404);
 
-    return res.send(customer.rows);
+    return res.send(customer.rows[0]);
   } catch (err) {
     return res.status(500).send(e.message);
+  };
+};
+
+export const createCustomer = async (req, res) => {
+  try {
+    const { name, phone, cpf, birthday } = req.body;
+
+    const existingCpf = await db.query('SELECT * FROM customers WHERE cpf = $1;', [cpf]);
+    if(existingCpf.rowCount > 0) return res.sendStatus(409);
+    
+    const newCustomer = await db.query(
+      `INSERT INTO 
+        customers (name, phone, cpf, birthday) 
+        VALUES($1, $2, $3, $4);
+      `, [name, phone, cpf, birthday]
+    );
+
+    return res.sendStatus(201);
+  } catch (err) {
+    return res.status(500).send(err.message);
   };
 };
--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import validateSchema from "../middlewares/validateSchema.js";
 import { customerSchema } from "../schemas/customers.schemas.js";
-import { listAllCustomers, listCustomerById } from "../controllers/customers.controllers.js";
+import { createCustomer, listAllCustomers, listCustomerById } from "../controllers/customers.controllers.js";
 
 const customersRouter = Router();
 customersRouter.get('/customers', listAllCustomers);
 customersRouter.get('/customers/:id', listCustomerById);
-customersRouter.post('/customers', validateSchema(customerSchema), );
+customersRouter.post('/customers', validateSchema(customerSchema), createCustomer);
 customersRouter.put('/customers', validateSchema(customerSchema), );
 
 export default customersRouter;
