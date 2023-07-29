b63bbd1eff9e95f2e6c2611055c7fe99724719b0
sayaoyuri <sayaoyuri.dev@gmail.com>
2023-07-29 20:32:52 -0300

    Feat(rentals) implementing post /rentals method

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -27,3 +27,39 @@ export const listAllRentals = async (req, res) => {
     return res.status(500).send(err.message);
   };
 };
+
+export const createRental = async (req, res) => {
+  try {
+    const { customerId, gameId, daysRented } = req.body;
+
+    const validCustomer = await db.query(`
+      SELECT * FROM customers WHERE id = $1;
+    `, [customerId]);
+
+    if(validCustomer.rowCount === 0) return res.status(400).send('Invalid user ID');
+
+    const validGame = await db.query(`
+      SELECT * FROM games WHERE id = $1 AND "stockTotal" > 0;
+    `, [gameId]);
+
+    if(validGame.rowCount === 0) return res.status(400).send('Invalid game ID or game is out of stock');
+  
+    const result = await db.query(`
+      INSERT INTO rentals 
+        ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") 
+        VALUES (
+          $1,
+          $2,
+          NOW(),
+          $3,
+          NULL, 
+          (SELECT SUM("pricePerDay" * $3) FROM games WHERE id = $2),
+          NULL
+        );
+    `, [customerId, gameId, daysRented]);
+  
+    return res.status(201).send();
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+}
--- src/routes/rentals.routes.js
+++ src/routes/rentals.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import validateSchema from "../middlewares/validateSchema.js";
 import { rentalSchema } from "../schemas/rentals.schemas.js";
-import { listAllRentals } from "../controllers/rentals.controllers.js";
+import { createRental, listAllRentals } from "../controllers/rentals.controllers.js";
 
 const rentalsRouter = Router();
 rentalsRouter.get('/rentals', listAllRentals);
-rentalsRouter.post('/rentals', validateSchema(rentalSchema), );
+rentalsRouter.post('/rentals', validateSchema(rentalSchema), createRental);
 rentalsRouter.post('/rentals/:id/return', );
 rentalsRouter.delete('/rentals/:id', );
 
